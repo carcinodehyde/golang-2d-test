e535aa6ecea957daa23b58724c6e323a9582b7b4
backend nebulabs <backend@nebulabs>
2021-03-13 20:25:55 +0700

    init ebiten & tiled

---
+++ assets/tiles/t.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="t" tilewidth="16" tileheight="16" tilecount="768" columns="32">
+ <image source="../images/tilea2.png" width="512" height="384"/>
+</tileset>
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/carcinodehyde/2d-test
+
+go 1.14
+
+require github.com/hajimehoshi/ebiten/v2 v2.0.6 // indirect
